4829052eeebf7be56a4cfb33578faba20dc96c22
Letícia Pinheiro <leticia.mpinheiro16@gmail.com>
2023-10-19 12:21:56 -0300

    Update desafio.kt
    desafio concluído

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,67 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario (val nome: String, val matricula: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, var duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuario: Usuario) {
+        inscritos.addAll(usuario)
+    }
+    
+    override fun toString(): String {
+        return """
+        $nome
+        Nível $nivel
+        ${conteudos.joinToString()}
+        Alunos: ${inscritos}""".trimIndent()
     }
 }
 
+fun gerarMatricula(): Int {
+    
+    val matriculas = mutableListOf<Int>()
+    var novaMatricula: Int
+    
+    do {
+   		novaMatricula = (1000..10000).random()
+	} while (novaMatricula in matriculas) 
+    
+    matriculas.add(novaMatricula)
+    return novaMatricula
+}
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val maria = Usuario("Maria", gerarMatricula())
+    val pedro = Usuario("Pedro", gerarMatricula())
+    val carla = Usuario("Carla", gerarMatricula())
+    val jose = Usuario("José", gerarMatricula())
+    val lucas = Usuario("Lucas", gerarMatricula())
+    
+    val caJava = ConteudoEducacional("Java", 45)
+    val caSpring = ConteudoEducacional("Spring", 60)
+    val caBancoDados = ConteudoEducacional("Banco de Dados", 90)
+    val caHTML = ConteudoEducacional("HTML", 30)
+    
+    val formSpring = Formacao("Curso Spring Framework", Nivel.BASICO, listOf(caSpring))
+    val formJava = Formacao("Curso Java", Nivel.INTERMEDIARIO, listOf(caJava))
+    val formWeb = Formacao("Curso Desenvolvedor Web", Nivel.AVANCADO, listOf(caJava, caSpring, caBancoDados, caHTML))
+    
+    formSpring.matricular(maria, pedro)
+    formJava.matricular(maria, carla, jose)
+    formWeb.matricular(carla, jose, lucas)
+    
+    println(formSpring)
+    println("\n----------------------\n")
+    println(formJava)
+    println("\n----------------------\n")
+    println(formWeb)
+    println("\n----------------------\n")
+    
 }
